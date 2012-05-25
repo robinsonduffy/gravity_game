06d7e91618234169fbff17f92725dc7fcc6617a4
Robinson Duffy <robinson.duffy@gmail.com>
2012-05-24 21:23:27 -0800

    Require cell and piece_type in game_pieces

--- app/models/game_piece.rb
+++ app/models/game_piece.rb
@@ -2,4 +2,7 @@ class GamePiece < ActiveRecord::Base
   belongs_to :level
   has_many :extra_attributes
   has_many :extra_classes
+  
+  validates :cell, :presence => true
+  validates :piece_type, :presence => true
 end
--- spec/models/game_piece_spec.rb
+++ spec/models/game_piece_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe GamePiece do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attrs = {:cell => '1,1', :piece_type => 'goal'}
+    @level = Factory(:level)
+  end
+  
+  it "should create a new game_piece given valid attributes" do
+    @level.game_pieces.create!(@attrs)
+  end
+  
+  describe "validations" do
+    it "should require a cell" do
+      bad_game_piece = @level.game_pieces.build(@attrs.merge({:cell => ''}))
+      bad_game_piece.should_not be_valid
+    end
+    
+    it "should require a piece_type" do
+      bad_game_piece = @level.game_pieces.build(@attrs.merge({:piece_type => ''}))
+      bad_game_piece.should_not be_valid
+    end
+    
+  end
 end
