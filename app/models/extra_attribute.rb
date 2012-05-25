a57d4381c307ad83a37317dc2b6387529a0fee56
Robinson Duffy <robinson.duffy@gmail.com>
2012-05-24 21:39:31 -0800

    Validations for extra_classes and extra_attributes

--- app/models/extra_attribute.rb
+++ app/models/extra_attribute.rb
@@ -1,3 +1,7 @@
 class ExtraAttribute < ActiveRecord::Base
   belongs_to :game_piece
+  
+  validates :key, :presence => true,
+                  :uniqueness => {:scope => :game_piece_id}
+  validates :value, :presence => true
 end
--- app/models/extra_class.rb
+++ app/models/extra_class.rb
@@ -1,3 +1,6 @@
 class ExtraClass < ActiveRecord::Base
   belongs_to :game_piece
+  
+  validates :name, :presence => true,
+                   :uniqueness => {:scope => :game_piece_id}
 end
