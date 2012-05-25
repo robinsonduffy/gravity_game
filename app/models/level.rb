6b0e58686d77cadf17c04f7b4310536d93f20ef0
Robinson Duffy <robinson.duffy@gmail.com>
2012-05-24 21:15:44 -0800

    Level validations

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,3 +1,8 @@
 class Level < ActiveRecord::Base
   has_many :game_pieces
+  
+  validates :number, :presence => true,
+                     :numericality => {:only_integer => true},
+                     :positive_number => true
+  
 end
--- spec/models/level_spec.rb
+++ spec/models/level_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe Level do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attrs = { :number => 1 }
+  end
+  
+  it "should create a new level given valid attributes" do
+    Level.create!(@attrs)
+  end
+  
+  describe "validations" do
+    it "should require a number" do
+      bad_level = Level.new(:number => '')
+      bad_level.should_not be_valid
+    end
+    
+    it "should require a positive number" do
+      bad_level = Level.new(:number => -1)
+      bad_level.should_not be_valid
+    end
+    
+    it "should require a numeric number" do
+      bad_level = Level.new(:number => 'a')
+      bad_level.should_not be_valid
+    end
+    
+    it "should require an integer" do
+      bad_level = Level.new(:number => 1.5)
+      bad_level.should_not be_valid
+    end
+  end
 end
