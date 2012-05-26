952a8fcb80659948d94929cf7e599cf8f4388a60
Robinson Duffy <robinson.duffy@gmail.com>
2012-05-26 13:43:26 -0800

    class_hash and attributes_hash methods

--- app/models/game_piece.rb
+++ app/models/game_piece.rb
@@ -5,4 +5,21 @@ class GamePiece < ActiveRecord::Base
   
   validates :cell, :presence => true
   validates :piece_type, :presence => true
+  
+  def class_hash
+    class_array = Array.new
+    class_array.push self.piece_type
+    self.extra_classes.each do |extra_class|
+      class_array.push extra_class.name
+    end
+    return {:class => class_array}
+  end
+  
+  def attributes_hash
+    attribute_hash = Hash.new
+    self.extra_attributes.each do |extra_attribute|
+      attribute_hash[extra_attribute.key] = extra_attribute.value
+    end
+    return attribute_hash
+  end
 end
--- spec/models/game_piece_spec.rb
+++ spec/models/game_piece_spec.rb
@@ -22,4 +22,31 @@ describe GamePiece do
     end
     
   end
+  
+  describe "special methods" do
+    before(:each) do
+      @game_piece = @level.game_pieces.create!(@attrs)
+      @game_piece.extra_classes.create!({:name => 'classA'})
+      @game_piece.extra_classes.create!({:name => 'classB'})
+      @game_piece.extra_attributes.create!({:key => '_attrA', :value => 'valueA'})
+      @game_piece.extra_attributes.create!({:key => '_attrB', :value => 'valueB'})
+      @game_piece.extra_attributes.create!({:key => '_attrC', :value => 'valueC'})
+    end
+    
+    it "should have a class_hash method" do
+      @game_piece.should respond_to(:class_hash)
+    end
+    
+    it "should return the class_hash when called" do
+      @game_piece.class_hash.should == {:class => ['goal', 'classA', 'classB']}
+    end
+    
+    it "should have a attributes_hash method" do
+      @game_piece.should respond_to(:attributes_hash)
+    end
+    
+    it "should return the attributes_hash when called" do
+      @game_piece.attributes_hash.should == {"_attrA" => 'valueA', "_attrB" => 'valueB', "_attrC" => 'valueC'}
+    end
+  end
 end
