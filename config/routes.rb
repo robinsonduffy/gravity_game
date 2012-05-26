d6e6ba14d51dd208a9f3ed4ae7e8060823776148
Robinson Duffy <robinson.duffy@gmail.com>
2012-05-26 13:59:30 -0800

    Basic level page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ GravityGame::Application.routes.draw do
   
   root :to => "pages#home"
   
+  match 'level/:id' => 'levels#show', :as => 'level_show'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
