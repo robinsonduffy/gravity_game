2a6ce57ce8d949da6277368148b82acf34cee540
Robinson Duffy <robinson.duffy@gmail.com>
2012-06-09 16:21:19 -0800

    Meta Data requires key and value

--- app/models/meta_datum.rb
+++ app/models/meta_datum.rb
@@ -1,3 +1,6 @@
 class MetaDatum < ActiveRecord::Base
   belongs_to :item, :polymorphic => true
+  
+  validates :key, :presence => true
+  validates :value, :presence => true
 end
--- spec/models/meta_datum_spec.rb
+++ spec/models/meta_datum_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe MetaDatum do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @level = Factory(:level)
+    @attrs = {:key => 'hello', :value => 'dolly'}
+  end
+  
+  it "should create a new meta_datum given proper attributes" do
+    @level.meta_data.create!(@attrs)
+  end
+  
+  describe "validations" do
+    it "should require a key" do
+      bad_meta_data = @level.meta_data.build(@attrs.merge({:key => ''}))
+      bad_meta_data.should_not be_valid
+    end
+    
+    it "should require a value" do
+      bad_meta_data = @level.meta_data.build(@attrs.merge({:value => ''}))
+      bad_meta_data.should_not be_valid
+    end
+  end
+  
 end
