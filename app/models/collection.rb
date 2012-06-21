4daec67268162174dd9de04e73da8cd255418c96
Robinson Duffy <robinson.duffy@gmail.com>
2012-06-20 21:50:17 -0800

    Percent done for collections

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -4,4 +4,12 @@ class Collection < ActiveRecord::Base
   validates :name, :presence => true
   validates :number, :presence => true,
                      :uniqueness => true
+                     
+  def percent_done(user = User.find(1))
+    levels_done = 0
+    self.levels.each do |level|
+      levels_done = levels_done + 1 if user.completed_levels.include?(level)
+    end
+    ((levels_done.to_f / self.levels.count.to_f) * 100).to_i
+  end
 end
