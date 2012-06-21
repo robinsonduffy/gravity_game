a56630e10e6f2c5c24d67aa6e1ab474f3b2c8966
Robinson Duffy <robinson.duffy@gmail.com>
2012-06-20 21:17:14 -0800

    Level index page grid

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,4 +1,6 @@
 class CollectionsController < ApplicationController
+  before_filter :require_get
+  before_filter :require_current_user
   
   def show
     @collection = Collection.find(params[:id])
