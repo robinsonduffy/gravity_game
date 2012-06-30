5a0402548752735b520d0fba2501630901868214
Robinson Duffy <robinson.duffy@gmail.com>
2012-06-30 14:00:30 -0800

    Development no longer requires facebook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,8 +34,12 @@ class ApplicationController < ActionController::Base
   end
   
   def require_current_user
-    @graph = Koala::Facebook::API.new(session[:access_token])
-    session[:fb_user_id] = (@graph.get_object('me'))["id"]
+    if Rails.env == 'production'
+      @graph = Koala::Facebook::API.new(session[:access_token])
+      session[:fb_user_id] = (@graph.get_object('me'))["id"]
+    else
+      session[:fb_user_id] = 'abcde12345'
+    end
     @game_user = User.find_by_fbid(session[:fb_user_id])
     if(@game_user.nil? && session[:fb_user_id].present? && !session[:fb_user_id].empty?)
       @game_user = User.create!({:fbid => session[:fb_user_id]})
