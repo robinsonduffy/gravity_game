58ce8b9fab936614b4d6ea881aab4645373e6e26
Robinson Duffy <robinson.duffy@gmail.com>
2012-07-09 20:13:45 -0800

    Removed no longer used methods, added coin methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,19 +7,17 @@ class User < ActiveRecord::Base
   validates :fbid, :presence => true,
                    :uniqueness => true
                    
-  def best_rotation(level)
-    self.completions.joins(:meta_data).where("meta_data.key = 'rotations' AND completions.level_id = ?", level.id).first.meta_data.find_by_key("rotations").value.to_i
-  end
-  
-  def best_locked(level)
-    self.completions.joins(:meta_data).where("meta_data.key = 'locks' AND completions.level_id = ?", level.id).first.meta_data.find_by_key("locks").value.to_i
-  end
-  
-  def best_coins(level)
-    self.completions.joins(:meta_data).where("meta_data.key = 'coins' AND completions.level_id = ?", level.id).first.meta_data.find_by_key("coins").value.to_i
-  end
-  
   def best_score(level)
     self.completions.joins(:meta_data).where("meta_data.key = 'score' AND completions.level_id = ?", level.id).first.meta_data.find_by_key("score").value.to_i
   end
+  
+  def add_coins(amount)
+    self.update_attribute(:coins, self.coins + amount)
+  end
+  
+  def remove_coins(amount)
+    amount = self.coins if amount > self.coins
+    self.update_attribute(:coins, self.coins - amount)
+  end
+  
 end
