81cbc643b52d6a19e90e0fd7b6d2fb5f4c9ecef6
Robinson Duffy <robinson.duffy@k12northstar.org>
2013-01-17 16:03:18 -0900

    Drag and Drop pieces & config dialogs

--- config/initializers/game_pieces.rb
+++ config/initializers/game_pieces.rb
@@ -12,63 +12,79 @@ def game_piece_info(piece_name)
       {
         :piece_type => 'game-piece',
         :lockable => false,
+        :configurable => false,
       }
     when "falling"
       {
         :piece_type => 'game-piece',
         :lockable => true,
-        :colors => game_piece_default_colors
+        :colors => game_piece_default_colors,
+        :configurable => true,
       }
     when "floating"
       {
         :piece_type => 'game-piece',
         :lockable => true,
-        :colors => game_piece_default_colors
+        :colors => game_piece_default_colors,
+        :configurable => true,
       }
     when "goal"
       {
         :piece_type => 'goal',
         :lockable => false,
-        :colors => game_piece_default_colors
+        :colors => game_piece_default_colors,
+        :configurable => true,
       }
     when "coin"
       {
         :piece_type => 'station',
         :lockable => false,
-        :colors => {
+        :colors => [
+          'green',
+          'blue',
+          'yellow',
+          'red'
+        ],
+        :color_values => {
           'green' => 1,
           'blue' => 5,
           'yellow' => 10,
           'red' => 25
-        }
+        },
+        :configurable => true,
       }
     when "bomb"
       {
         :piece_type => 'game-piece',
         :lockable => false,
-        :colors => game_piece_default_colors
+        :colors => game_piece_default_colors,
+        :configurable => true,
       }
     when "paint"
       {
         :piece_type => 'station',
         :lockable => false,
-        :colors => game_piece_default_colors
+        :colors => game_piece_default_colors,
+        :configurable => true,
       }
     when "gravity-swap"
       {
         :piece_type => 'station',
         :lockable => false,
+        :configurable => false,
       }
     when "magnet"
       {
         :piece_type => 'station',
         :lockable => false,
+        :configurable => false,
       }
     when "teleport"
       {
         :piece_type => 'station',
         :lockable => false,
-        :colors => game_piece_default_colors
+        :colors => game_piece_default_colors,
+        :configurable => false,
       }
     else
       nil
--- public/javascripts/factory.js
+++ public/javascripts/factory.js
@@ -8,23 +8,132 @@ $(document).ready(function(){
     }
   });
 
+  $("#board").on("change",function(){
+    disable_or_enable_teleport_drag();
+    recolor_teleports();
+  });
+
+  $("#board").on("resize", function(){
+    initialize_drop();
+  });
+
   $("#grid-size").change(function(){
     new_grid_size = $(this).val();
     $("#board").css('visibility', 'hidden');
     rebuild_grid(new_grid_size);
     adjust_game_pieces_to_fit_grid(new_grid_size);
-    $("#table-grid-size-css").attr("href", "/stylesheets/table-"+new_grid_size+".css");
+    $('#board-wrapper').removeClass().addClass('grid-size-'+new_grid_size);
+    setUpPieces();
     $("#board").css('visibility', 'visible');
-    if(max_x > new_grid_size){
-      //we are making the grid smaller
-    }else if(max_x < new_grid_size){
-      //we are making the grid bigger
+    $("#board").trigger("change");
+  });
 
-    }else{
-      //the grid is staying the same size?  for some reason
-    }
+  $("#add-game-pieces").on("click","p.disabled",function(){
+    //TODO:Allow users to purchase access to disabled level elements
 
   });
+
+  $("input[name='mode']").change(function(){
+    if($("input[name='mode']:checked").val() == 'play'){
+      $("p.rotate").css('visibility','');
+      $("#game-pieces div.lockable-disabled").removeClass("lockable-disabled").addClass("lockable");
+      $("#factory-edit-controls").css('visibility','hidden');
+      $("#game-pieces div").addClass('no-drag no-click');
+      $("#game-pieces div.configurable").removeClass("configurable").addClass("configurable-disabled");
+      applyGravity();
+    }else{
+      $("p.rotate").css('visibility','hidden');
+      $("#game-pieces div.lockable").removeClass("lockable").addClass("lockable-disabled");
+      $("#factory-edit-controls").css('visibility','');
+      $("#game-pieces div").removeClass('no-drag no-click');
+      $("#game-pieces div.configurable-disabled").removeClass("configurable-disabled").addClass("configurable");
+    }
+  });
+
+  $("#game-pieces").on("dblclick",'div:not(.no-click)',function(){
+    $(this).addClass('currently-being-configured');
+    game_piece_info = $(".add-game-piece[data-game_piece='"+$(this).data("game_piece")+"']").data("game_piece_info")
+    if($(this).hasClass("configurable")){
+      $("#game-piece-dialog #save-game-piece").show();
+      $("#game-piece-dialog #game-piece-config-options").show();
+      $("#game-piece-config-options #color select").empty();
+      if($(this).data("game_piece") == 'coin'){
+        $("#game-piece-config-options #color label").html("Value");
+        for(x in game_piece_info.colors){
+          option = $("<option></option>");
+          option.html(game_piece_info.color_values[game_piece_info.colors[x]]).val(game_piece_info.colors[x]);
+          if(game_piece_info.colors[x] == $(this).attr("_color")){
+            option.attr("selected", "selected");
+          }
+          option.appendTo("#game-piece-config-options #color select");
+        }
+      }else{
+        $("#game-piece-config-options #color label").html("Color");
+        for(x in game_piece_info.colors){
+          option = $('<option></option>');
+          option.html(game_piece_info.colors[x].toUpperCase()).val(game_piece_info.colors[x]);
+          if(game_piece_info.colors[x] == $(this).attr("_color")){
+            option.attr("selected", "selected");
+          }
+          option.appendTo("#game-piece-config-options #color select");
+        }
+      }
+      if($(this).hasClass("lockable-disabled")){
+        $("#game-piece-config-options #locked").show();
+        if($(this).hasClass("locked")){
+          $("#game-piece-config-options #locked input").attr("checked","checked");
+        }else{
+          $("#game-piece-config-options #locked input").removeAttr("checked");
+        }
+      }else{
+        $("#game-piece-config-options #locked").hide();
+      }
+    }else{
+      $("#game-piece-dialog #save-game-piece").hide();
+      $("#game-piece-dialog #game-piece-config-options").hide();
+    }
+    $("#game-piece-dialog").dialog('open');
+  });
+
+  $("#game-piece-dialog").dialog({
+    autoOpen: false,
+    modal: true,
+    draggable: false,
+    resizable: false,
+    title: "Game Piece Settings",
+    close: function(){
+      $('.currently-being-configured').removeClass("currently-being-configured");
+    }
+  });
+
+  $("#delete-game-piece").click(function(){
+    $(".currently-being-configured").remove();
+    $("#game-piece-dialog").dialog("close");
+  });
+
+  $("#save-game-piece").click(function(){
+    piece = $(".currently-being-configured").eq(0);
+    game_piece_info = $(".add-game-piece[data-game_piece='"+piece.data("game_piece")+"']").data("game_piece_info")
+    old_color = piece.attr("_color");
+    new_color = $("#game-piece-dialog #game-piece-config-options #color option:selected").val();
+    piece.attr("_color", new_color).removeClass(old_color).addClass(new_color);
+    if(piece.data("game_piece") == 'coin'){
+      piece.attr("_coin_value", game_piece_info["color_values"][new_color]);
+    }
+    if(piece.hasClass("lockable-disabled")){
+      if($("#game-piece-dialog #game-piece-config-options #locked input").prop("checked")){
+        piece.addClass("locked");
+      }else{
+        piece.removeClass("locked");
+      }
+    }
+    $("#game-piece-dialog").dialog("close");
+  });
+
+  $("#board").trigger("change");
+  initialize_drag();
+  initialize_drop();
+  $("input[name='mode']").trigger("change");
 });
 
 function rebuild_grid(grid_size){
@@ -38,16 +147,152 @@ function rebuild_grid(grid_size){
     }
     tr.appendTo("#board table#squares");
   }
+  max_x = parseInt($("#board table tr").length);
+  max_y = parseInt($("#board table tr").length);
+  $("#board").trigger("resize");
 }
 
 function adjust_game_pieces_to_fit_grid(grid_size){
-  $("#board .game-piece").addClass("adjust-grid-delete");
+  $("#board #game-pieces div").addClass("adjust-grid-delete");
   //mark the pieces we are going to keep...
-  for(var i=0; i < grid_size; i++){
-    for(var j=0; j < grid_size; j++){
-      $("#board .game-piece[_cell='"+i+","+j+"']").removeClass("adjust-grid-delete");
+  for(var i=1; i <= grid_size; i++){
+    for(var j=1; j <= grid_size; j++){
+      $("#board #game-pieces div[_cell='"+i+","+j+"']").removeClass("adjust-grid-delete");
     }
   }
   //get rid of the rest...
-  $("#board .game-piece.adjust-grid-delete").remove();
+  $("#board #game-pieces div.adjust-grid-delete").each(function(){
+    $(this).remove();
+    if($(this).hasClass("teleport")){
+      //get rid of linked teleports
+      $("div[_teleport_exit_cell='"+$(this).attr('_cell')+"']").remove();
+    }
+  });
+}
+
+function initialize_drag(){
+  $("#add-game-pieces p.usable").draggable({
+    zIndex: '1000',
+    cancel: '.no-drag',
+    helper: function(event){
+      game_piece_info = $(this).data("game_piece_info")
+      new_piece = $("<div></div>");
+      new_piece.addClass(game_piece_info.piece_type).addClass($(this).data("game_piece"));
+      new_piece.data("game_piece", $(this).data("game_piece"));
+      if(game_piece_info.lockable){
+        new_piece.addClass('lockable-disabled');
+      }
+      if(game_piece_info.configurable){
+        new_piece.addClass('configurable init-config');
+      }
+      if($.inArray($(this).data("game_piece"),["falling","floating","goal","coin","bomb","paint"]) >= 0){
+        //these elements are pre-assigned the first color in the list
+        new_piece.addClass(game_piece_info.colors[0]).attr("_color",game_piece_info.colors[0]);
+      }
+      if($(this).data("game_piece") == 'teleport'){
+        //select the next color automatically based on how many teleports there already are.
+        num_of_teleports = $("#game-pieces .teleport").size();
+        if(num_of_teleports % 2){
+          num_of_teleports--;
+        }
+        teleport_color = num_of_teleports / 2;
+        new_piece.addClass(game_piece_info.colors[teleport_color]).attr('_color', game_piece_info.colors[teleport_color]);
+      }
+      if($(this).data("game_piece") == 'coin'){
+        new_piece.attr("_coin_value", game_piece_info.color_values[game_piece_info.colors[0]]);
+      }
+      console.log($(this).data("game_piece_info"));
+      new_piece.appendTo("#game-pieces");
+      return new_piece;
+    }
+  });
+
+  $("#game-pieces div").draggable({
+    zIndex: 1000,
+    cancel: '.no-drag',
+    revert: 'invalid',
+    revertDuration: 100
+  });
+}
+
+function initialize_drop(){
+  $("#squares td").droppable({
+    hoverClass: 'drag-and-drop-target-hover',
+    //accept: $(":not[.bad-element]"),
+    accept: function(){
+      cell = $(this).attr("_cell");
+      return $("#board #game-pieces div[_cell='"+cell+"']").length < 1
+    },
+    drop: function(event,ui){
+      //is this block empty?
+      cell = $(this).attr("_cell");
+      if($("#board .game-piece[_cell='"+cell+"']").length < 1 && $("#board .station[_cell='"+cell+"']").length < 1){
+        if(ui.helper != ui.draggable){
+          console.log("new piece");
+          new_piece = $(ui.helper).clone(true);
+          $(ui.helper).remove();
+          new_piece.removeClass("ui-draggable-dragging").css("z-index", '').appendTo("#game-pieces");
+          new_piece.draggable({
+            zIndex: 1000,
+            cancel: '.no-drag',
+            revert: 'invalid',
+            revertDuration: 100
+          });
+        }else{
+          console.log("Existing Piece");
+          new_piece = ui.draggable;
+        }
+        new_piece.attr("_cell", cell)
+        if(new_piece.hasClass("teleport")){
+          //check to see if there is one or two teleports of this color
+          if($("#game-pieces div.teleport."+new_piece.attr('_color')).length == 1){
+            new_piece.removeAttr('_teleport_exit_cell');
+          }else{
+            new_piece.addClass('teleport_new_piece');
+            $("#game-pieces div.teleport."+new_piece.attr('_color')+':not(.teleport_new_piece)').attr('_teleport_exit_cell', new_piece.attr('_cell'));
+            new_piece.attr('_teleport_exit_cell',$("#game-pieces div.teleport."+new_piece.attr('_color')+':not(.teleport_new_piece)').attr('_cell'));
+            new_piece.removeClass('teleport_new_piece');
+          }
+        }
+        new_piece.css({
+          left: $("#board .square[_cell='"+cell+"']").position().left+'px',
+          top: $("#board .square[_cell='"+cell+"']").position().top+'px'
+        });
+        $("#board").trigger("change");
+        if(new_piece.hasClass("init-config")){
+          new_piece.trigger("dblclick");
+          new_piece.removeClass("init-config");
+        }
+      }
+    }
+  });
+}
+
+function disable_or_enable_teleport_drag(){
+  num_of_teleports = $("#game-pieces .teleport").size();
+  if(num_of_teleports % 2){
+    num_of_teleports--;
+  }
+  teleport_color = num_of_teleports / 2;
+  game_piece_info = $("[data-game_piece=teleport]").data("game_piece_info");
+  if(teleport_color >= game_piece_info.colors.length){
+    $("[data-game_piece=teleport]").addClass('no-drag');
+  }else{
+    $("[data-game_piece=teleport]").removeClass('no-drag');
+  }
+}
+
+function recolor_teleports(){
+  var current_color = 0;
+  game_piece_info = $("[data-game_piece=teleport]").data("game_piece_info");
+  $("#game-pieces div.teleport").each(function(){
+    if(!$(this).hasClass('already-changed-color')){
+      old_color = $(this).attr('_color');
+      $(this).addClass('already-changed-color');
+      $(this).removeClass(old_color).addClass(game_piece_info.colors[current_color]).attr('_color',game_piece_info.colors[current_color]);
+      $("#game-pieces div.teleport[_cell='"+$(this).attr('_teleport_exit_cell')+"']").addClass('already-changed-color').removeClass(old_color).addClass(game_piece_info.colors[current_color]).attr('_color',game_piece_info.colors[current_color]);
+      current_color++;
+    }
+  });
+  $("div.teleport.already-changed-color").removeClass('already-changed-color');
 }
