d1d442208a8710bf3bb570e9848bb7fef11b5437
Robinson Duffy <robinson.duffy@k12northstar.org>
2013-01-17 15:58:33 -0900

    Require user to access level factory

--- app/controllers/level_factory_controller.rb
+++ app/controllers/level_factory_controller.rb
@@ -1,5 +1,7 @@
 class LevelFactoryController < ApplicationController
 
+  before_filter :require_current_user
+
   def new
     @title = "Level Factory"
     @level = Level.new(:collection_id => '0', :published => false, :grid_size => 4)
