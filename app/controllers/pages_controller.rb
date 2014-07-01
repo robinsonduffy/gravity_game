b59d11e97cde1f78f70c3ebbe80b2b530e27d3d6
Robinson Duffy <robinson.duffy@gmail.com>
2014-07-01 17:49:37 -0500

    Getting rid of some FB cruft...

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,10 @@
 class PagesController < ApplicationController
   
   before_filter :require_login, :only => [:home]
-  before_filter :redirect_after_canvas_post, :only => [:home]
   before_filter :clear_current_level
-  protect_from_forgery :except => [:home]
   
   def home
     @collections = Collection.order(:number)
-  end
-  
-  def redirect_after_canvas_post
-    if request.method == 'GET'
-      unless session[:post_return_to].nil?
-        redirect_to session[:post_return_to] and session[:post_return_to] = nil and return
-      end
-    end
   end
   
 end
